e2f6e6905d29ea5de7683bc1d978d2a2c8fdcd80
ploidt <ploid@Ploid.local>
2014-10-14 21:40:35 +0700

    first commit

---
+++ lib/game.rb
@@ -0,0 +1,47 @@
+class Game
+  FRAME = 10
+
+  def initialize
+    @throws = []
+  end
+
+  def roll(pins)
+    @throws.push pins
+  end
+
+  def score
+    score = 0
+    index = 0
+
+    FRAME.times do
+      if strike?(index)
+        score += strike_score(index)
+        index += 1
+      elsif spare?(index)
+        score += spare_score(index)
+        index += 2
+      else
+        score += (@throws[index] + @throws[index+1])
+        index += 2
+      end
+    end
+    score
+  end
+
+  private
+  def strike?(index)
+    @throws[index] == 10
+  end
+
+  def spare?(index)
+    @throws[index] + @throws[index+1] == 10
+  end
+
+  def strike_score(index)
+    10 + @throws[index+1] + @throws[index+2]
+  end
+
+  def spare_score(index)
+    10 + @throws[index+2]
+  end
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,36 @@
+require "game"
+
+describe Game do
+  describe '#score' do
+    before do
+      @game = Game.new
+    end
+
+    it 'returns 300 for all strike' do
+      12.times { @game.roll(10) }
+      temp = @game.score
+      expect(temp).to eq(300)
+    end
+
+    it 'retunrs 172 for test' do
+      @game.roll(10)
+      @game.roll(8)
+      @game.roll(1)
+      @game.roll(2)
+      @game.roll(8)
+      @game.roll(5)
+      @game.roll(4)
+      @game.roll(10)
+      @game.roll(10)
+      @game.roll(10)
+      @game.roll(5)
+      @game.roll(5)
+      @game.roll(10)
+      @game.roll(10)
+      2.times{ @game.roll(0)}
+      temp = @game.score
+      expect(temp) == 172
+    end
+
+  end
+end
