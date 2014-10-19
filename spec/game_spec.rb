97b7e12417ec542dc1512a8103e018470a3e237f
ploidt <ploid@Ploid.local>
2014-10-19 16:43:10 +0700

    final

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,7 @@
 
 require "game"
 
-describe Game do
+describe "Test Game" do
   describe '#score' do
     before do
       @game = Game.new
@@ -11,9 +11,12 @@ describe Game do
       12.times { @game.roll(10) }
       temp = @game.score
       expect(temp).to eq(300)
+      puts "************************ \n\n"
+      puts "Total score : #{temp}"
     end
 
-    it 'returns 177 for test' do
+    it 'returns 177 for in-class case' do
+      puts "\n\n"
       @game.roll(10)
       @game.roll(8)
       @game.roll(1)
@@ -31,6 +34,34 @@ describe Game do
       2.times{ @game.roll(0)}
       temp = @game.score
       expect(temp).to eq(177)
+      puts "************************ \n\n"
+      puts "Total score : #{temp}"
+    end
+
+    it 'return 133 for more case' do
+      @game.roll(1)
+      @game.roll(4)
+      @game.roll(4)
+      @game.roll(5)
+      @game.roll(6)
+      @game.roll(4)
+      @game.roll(5)
+      @game.roll(5)
+      @game.roll(10)
+      @game.roll(0)
+      @game.roll(1)
+      @game.roll(7)
+      @game.roll(3)
+      @game.roll(6)
+      @game.roll(4)
+      @game.roll(10)
+      @game.roll(2)
+      @game.roll(10)
+      @game.roll(6)
+      temp = @game.score
+      expect(temp).to eq(131)
+      puts "************************ \n\n"
+      puts "Total score : #{temp}"
     end
 
   end
